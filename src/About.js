d1e9fef49a6bb41f34559a571ce75bdc5b645926
cwalbanese <631gti@gmail.com>
2020-01-27 15:56:50 -0500

    Modified about.js
    Created content for and styled About page

--- src/About.js
+++ src/About.js
@@ -1,8 +1,27 @@
 import React, { Component } from 'react';
+import './App.css';
 
 class About extends Component {
   render() {
-    return <div>about</div>;
+    return (
+      <div>
+        <img
+          className="bartender"
+          src="./images/bartender.jpg"
+          alt="bartender"
+        ></img>
+        <p>
+          <span className="thirsty">thirsty</span> is an application designed to
+          assist you when, well, you're
+          <span className="thirsty"> thirsty</span>. it is a way to search for
+          local breweries, cideries, brewpubs, and bottleshops. instead of
+          scouring google trying to figure out what's a brewery and what's a
+          bar, use the <span className="thirsty"> thirsty</span> app to filter
+          your search to just breweries and alike! how cool is that? happy
+          drinking and be safe.
+        </p>
+      </div>
+    );
   }
 }
 
